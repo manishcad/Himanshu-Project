58cd539a5cc7d65fede14da8e61447a37f6cc9cd
Manish Chand <manishtochand@gmail.com>
2023-02-13 22:43:02 +0530

    Update settings.py

--- books/settings.py
+++ books/settings.py
@@ -29,7 +29,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
-CSRF_TRUSTED_ORIGINS = ["https://destructive-protfolio.up.railway.app"]
+CSRF_TRUSTED_ORIGINS = ["https://manga-world.up.railway.app"]
 # Application definition
 
 INSTALLED_APPS = [
